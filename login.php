00d18ee3fc4a872ed53a75808fe79508ca09f591
nutfyb <56430739+nutfyb@users.noreply.github.com>
2022-01-25 01:09:27 +0700

    login and regiter page

---
+++ login.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@300&family=Prompt:wght@500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@300&family=Prompt:ital,wght@1,300&display=swap" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="mystyle.css">
+    <title>เข้าสู่ระบบ | สมาคมศิษย์เก่าวิทยาลัยเทคโนโลยีทางการแพทย์และสาธารณสุขกาญจนาภิเษก</title>
+</head>
+
+<body style="background-image: url('Picture/flower2.jpg');">
+    <div class="container-login">
+        <div class="container-box">
+            <img src="Picture/logo2.png" alt="" width="100" height="140">
+            <h6 class="m-b-37" style="text-align: center;font-family: 'Kanit', sans-serif;">สมาคมศิษย์เก่าวิทยาลัยเทคโนโลยีทางการแพทย์และสาธารณสุขกาญจนาภิเษก</h6>
+            <h2 class="m-b-37" style="font-family: 'Prompt', sans-serif;">เข้าสู่ระบบ</h2>
+            <input type="text" class="form-input m-b-20" id="formGroupExampleInput" placeholder="บัญชีผู้ใช้" style="text-align: center;font-family: 'Kanit', sans-serif;">
+            <input type="password" class="form-input m-b-27" id="inputPassword3" placeholder="รหัสผ่าน" style="text-align: center;font-family: 'Kanit', sans-serif;">
+            <button type="button" class="btn" style="text-align: center;font-family: 'Kanit', sans-serif;">เข้าสู่ระบบ</button>
+            <p class="m-b-100" style="text-align: center;font-family: 'Kanit', sans-serif;">หรือคุณยังไม่มีบัญชี</p>
+            <a href="/guitar/regiterpage.php"style="text-align: center;font-family: 'Kanit', sans-serif;color:#000">ลงทะเบียน</a>
+        </div>
+    </div>
+
+</body>
+
+<!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+<!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    -->
+
+</html>
